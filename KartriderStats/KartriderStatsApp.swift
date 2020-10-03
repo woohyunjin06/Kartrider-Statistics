5fec22acc85e4bdc67415c59bb0ff77b43eca4bb
woohyunjin06 <woohyunjin06@gmail.com>
2020-10-03 14:39:04 +0900

    Initial Commit

---
+++ KartriderStats/KartriderStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KartriderStatsApp.swift
+//  KartriderStats
+//
+//  Created by 현진 on 2020/10/03.
+//
+
+import SwiftUI
+
+@main
+struct KartriderStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
